62b2bcde762a575f219bca0053edf5081c8c12da
Freddi <mail@FrederikHeinrich.de>
2019-10-17 00:37:55 +0200

    fixing footer menu..
    but still not centerd...

--- FrontEnd/src/pages/Footer.js
+++ FrontEnd/src/pages/Footer.js
@@ -8,13 +8,34 @@ class Footer extends React.Component {
   render(){
     return (
       <div className="footer">
-        <Image
-        src='/img/settings.png'
-        alt='settings'
-        onClick={() => {
-          UserStore.page = "settings";
-        }}
-        />
+      <Image
+      src='/img/lamph.png'
+      alt='home'
+      onClick={() => {
+        UserStore.page = "home";
+      }}
+      />
+      <Image
+      src='/img/temperature.png'
+      alt='temperature'
+      onClick={() => {
+        UserStore.page = "temperature";
+      }}
+      />
+      <Image
+      src='/img/shortcuts.png'
+      alt='shortcuts'
+      onClick={() => {
+        UserStore.page = "shortcuts";
+      }}
+      />
+      <Image
+      src='/img/settings.png'
+      alt='settings'
+      onClick={() => {
+        UserStore.page = "settings";
+      }}
+      />
           
       </div>
     );
